a3865cf8a21960dd25d9b73b03ca17d1f6cc7781
victoriab19 <victoriawork111@gmail.com>
2014-05-19 12:10:06 +0100

    Create methods.rb

---
+++ methods.rb
@@ -0,0 +1,18 @@
+# A simple method  
+def hello  
+  'Hello'  
+end  
+#use the method  
+puts hello  
+  
+# Method with an argument - 1  
+def hello1(name)  
+  'Hello ' + name  
+end  
+puts(hello1('omais'))  
+  
+# Method with an argument - 2  
+def hello2 name2  
+  'Hello ' + name2  
+end  
+puts(hello2 'ali') 
